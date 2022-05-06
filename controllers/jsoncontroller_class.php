9e46a4c23c08b69555e4dccf22a7202cee41ad69
Romanets Pavel <pavelroma24@gmail.com>
2022-05-06 10:44:11 +0300

    up: header Access-Control-Allow-Origin

--- controllers/jsoncontroller_class.php
+++ controllers/jsoncontroller_class.php
@@ -2,6 +2,7 @@
     class JsonController extends Controller {
         public function __construct() {
             parent::__construct();
+            header('Access-Control-Allow-Origin: *');
             header('Content-Type: application/json; charset=utf-8');
         }
     }
